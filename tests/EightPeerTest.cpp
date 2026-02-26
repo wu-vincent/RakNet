2a89d3abb0cc54d8098bbd59ad95d9d3436da8df
Vincent <magicdroidx@gmail.com>
2026-02-26 01:07:30 +0000

    test: migrate MaximumConnectTest and EightPeerTest to gtest
    - MaximumConnectTest: verify max incoming connection limits (SetMaximumIncomingConnections).
    - EightPeerTest: validate full mesh connectivity and reliable ordered delivery across 8 peers with 100 packets each.
    - Replace legacy GetTime/GetTimeMS calls with std::chrono for deadlines and sleep.
    - Add doxygen @brief comments for both test fixtures.
    - Update CMakeLists to include new test files.

--- tests/EightPeerTest.cpp
+++ tests/EightPeerTest.cpp
@@ -3,376 +3,170 @@
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  LICENSE file in the root directory of this source tree. An additional grant
  *  of patent rights can be found in the PATENTS file in the same directory.
  *
  */
 
-#include "EightPeerTest.h"
-
-/*
-What is being done here is having 8 peers all connect to eachother and be
-connected. Then it check if they all connect. If so send data in ordered reliable mode for 100
-loops.
-
-Possible ideas for changes:
-Possibly use rakpeerinterfaces GetSystemList() for number of 
-connected peers instead of manually tracking. Would be slower though,
-shouldn't be significant at this number but the recieve speed it part of the test.
-
-Success conditions:
-Peers connect and receive all packets in order.
-No disconnections allowed in this version of the test.
-
-Failure conditions:
-
-If cannot connect to all peers for 20 seconds.
-All packets are not recieved.
-All packets are not in order.
-Disconnection.
-*/
-int EightPeerTest::RunTest(DataStructures::List<RakString> params,bool isVerbose,bool noPauses)
-{
-	const int peerNum= 8;
-	RakPeerInterface *peerList[peerNum];//A list of 8 peers
-	int connectionAmount[peerNum];//Counter for me to keep track of connection requests and accepts
-	int recievedFromList[peerNum][peerNum];//Counter for me to keep track of packets received
-	int lastNumberReceivedFromList[peerNum][peerNum];//Counter for me to keep track of last recieved sequence number
-	const int numPackets=100;
-	Packet *packet;
-	BitStream bitStream;
-	destroyList.Clear(false,_FILE_AND_LINE_);
-
-	//Initializations of the arrays
-	for (int i=0;i<peerNum;i++)
-	{
-		peerList[i]=RakPeerInterface::GetInstance();
-		destroyList.Push(peerList[i],_FILE_AND_LINE_);
-		connectionAmount[i]=0;
-
-		for (int j=0;j<peerNum;j++)
-		{
-			recievedFromList[i][j]=0;
-			lastNumberReceivedFromList[i][j]=0;
-		}
-
-		peerList[i]->Startup(peerNum*2, &SocketDescriptor(60000+i,0), 1);
-		peerList[i]->SetMaximumIncomingConnections(peerNum);
-
-	}
-
-	//Connect all the peers together
-	for (int i=0;i<peerNum;i++)
-	{
-		for (int j=i+1;j<peerNum;j++)//Start at i+1 so don't connect two of the same together.
-		{
-			if (peerList[i]->Connect("127.0.0.1", 60000+j, 0,0)!=CONNECTION_ATTEMPT_STARTED)
-			{
-				if (isVerbose)
-				{
-					DebugTools::ShowError("Problem while calling connect. \n",!noPauses && isVerbose,__LINE__,__FILE__);
-
-				}
-				return 1;//This fails the test, don't bother going on.
-			}
-		}
-
-	}
-
-	TimeMS entryTime=GetTimeMS();//Loop entry time
-	TimeMS finishTimer=GetTimeMS();
-	bool initialConnectOver=false;//Our initial connect all has been done.
-
-	for (int k=0;k<numPackets||GetTimeMS()-finishTimer<5000;)//Quit after we send 100 messages while connected, if not all connected and not failure, otherwise fail after 20 seconds and exit
-	{
-		bool allConnected=true;//Start true, only one failed case makes it all fail
-		for (int i=0;i<peerNum;i++)//Make sure all peers are connected to eachother
-		{
-			if (connectionAmount[i]<peerNum-1)
-			{
-				allConnected=false;
-			}
-		}
-
-		if (GetTimeMS()-entryTime>20000 &&!initialConnectOver &&!allConnected)//failed for 20 seconds
-		{
-
-			if (isVerbose)
-				DebugTools::ShowError("Failed to connect to all peers after 20 seconds",!noPauses && isVerbose,__LINE__,__FILE__);
-			return 2;
-			break;
-		}
-
-		if (allConnected)
-		{
-			if(!initialConnectOver)
-				initialConnectOver=true;
-			if (k<numPackets)
-			{
-			for (int i=0;i<peerNum;i++)//Have all peers send a message to all peers
-			{
-
-				bitStream.Reset();
-
-				bitStream.Write((unsigned char) (ID_USER_PACKET_ENUM+1));
-
-				bitStream.Write(k);
-				bitStream.Write(i);
-
-				peerList[i]->Send(&bitStream, HIGH_PRIORITY, RELIABLE_ORDERED ,0, UNASSIGNED_SYSTEM_ADDRESS, true);
-
-			}
-			}
-			k++;
-		}
-
-		if (k>=numPackets-3)//This is our last 3 packets, give it time to send packet and arrive on interface, 2 seconds is more than enough 
-		{
-			RakSleep(300);
-			if (k==numPackets)
-			{
-				finishTimer=GetTimeMS();
-			}
-		}
-
-		for (int i=0;i<peerNum;i++)//Receive for all peers
-		{
-			if (allConnected)//If all connected try to make the data more visually appealing by bunching it in one receive
-			{
-				int waittime=0;
-				do
-				{
-					packet=peerList[i]->Receive();
-					waittime++;
-
-					if (!packet)
-					{
-						RakSleep(1);
-
-					}
-
-					if (waittime>1000)//Check for packet every millisec and if one second has passed move on, don't block execution
-					{
-						break;
-					}
-				}
-				while(!packet);//For testing purposes wait for packet a little while, go if not recieved
-			}
-			else//Otherwise just keep recieving quickly until connected
-			{
-				packet=peerList[i]->Receive();
-			}
-			if (isVerbose)
-				printf("For peer %i with %i connected peers.\n",i,connectionAmount[i]);
-			while(packet)
-			{
-				switch (packet->data[0])
-				{
-				case ID_REMOTE_DISCONNECTION_NOTIFICATION:
-					if (isVerbose)
-					{
-						printf("Another client has disconnected.\n");
-						DebugTools::ShowError("Test failed.\n",!noPauses && isVerbose,__LINE__,__FILE__);
-					}
-					return 3;
-					break;
-				case ID_REMOTE_CONNECTION_LOST:
-					if (isVerbose)
-					{
-						printf("Another client has lost the connection.\n");
-						DebugTools::ShowError("Test failed.\n",!noPauses && isVerbose,__LINE__,__FILE__);           
-					}
-					return 3;
-					break;
-				case ID_REMOTE_NEW_INCOMING_CONNECTION:
-					if (isVerbose)              
-						printf("Another client has connected.\n");
-					break;
-				case ID_CONNECTION_REQUEST_ACCEPTED:
-					if (isVerbose)              
-						printf("Our connection request has been accepted.\n");
-					connectionAmount[i]++;
-
-					break;
-				case ID_CONNECTION_ATTEMPT_FAILED:
-
-					if (isVerbose)
-						DebugTools::ShowError("A connection has failed.\n Test failed.\n",!noPauses && isVerbose,__LINE__,__FILE__);
-					return 2;
-					break;
-
-				case ID_NEW_INCOMING_CONNECTION:
-					if (isVerbose)              
-						printf("A connection is incoming.\n");
-					connectionAmount[i]++;//For this test assume connection. Test will fail if connection fails.
-					break;
-				case ID_NO_FREE_INCOMING_CONNECTIONS://Should not happend
-					if (isVerbose)    
-					{
-						printf("The server is full. This shouldn't happen in this test ever.\n");
-
-						DebugTools::ShowError("Test failed.\n",!noPauses && isVerbose,__LINE__,__FILE__);
-					}
-					return 2;
-					break;
-
-				case ID_ALREADY_CONNECTED:
-					if (isVerbose)              
-						printf("Already connected\n");//Shouldn't happen
-
-					break;
-
-				case ID_DISCONNECTION_NOTIFICATION:
-					if (isVerbose)
-					{
-						printf("We have been disconnected.\n");
-
-						DebugTools::ShowError("Test failed.\n",!noPauses && isVerbose,__LINE__,__FILE__);
-					}
-					return 3;
-					break;
-				case ID_CONNECTION_LOST:
-					allConnected=false;
-					connectionAmount[i]--;
-					if (isVerbose)
-					{
-						printf("Connection lost.\n");
-
-						DebugTools::ShowError("Test failed.\n",!noPauses && isVerbose,__LINE__,__FILE__);
-					}
-
-					return 3;
-
-					break;
-				default:
-
-					if (packet->data[0]==ID_USER_PACKET_ENUM+1)
-					{
-						int thePeerNum;
-						int sequenceNum;
-						bitStream.Reset();
-						bitStream.Write((char*)packet->data, packet->length);
-						bitStream.IgnoreBits(8);
-						bitStream.Read(sequenceNum);
-						bitStream.Read(thePeerNum);
-						if (isVerbose)
-							printf("Message %i from %i\n",sequenceNum,thePeerNum );
-
-						if (thePeerNum>=0&&thePeerNum<peerNum)
-						{
-							if (lastNumberReceivedFromList[i][thePeerNum]==sequenceNum)
-							{
-								lastNumberReceivedFromList[i][thePeerNum]++;
-							}
-							else
-							{
-								if (isVerbose)
-								{
-									printf("Packets out of order");
-									DebugTools::ShowError("Test failed.\n",!noPauses && isVerbose,__LINE__,__FILE__);
-
-								}
-								return 4;
-							}
-							recievedFromList[i][thePeerNum]++;}
-					}
-					break;
-				}
-				peerList[i]->DeallocatePacket(packet);
-				// Stay in the loop as long as there are more packets.
-				packet = peerList[i]->Receive();
-			}
-		}
-		RakSleep(0);//If needed for testing
-	}
-
-	for (int i=0;i<peerNum;i++)
-	{
-
-		for (int j=0;j<peerNum;j++)
-		{
-			if (i!=j)
-			{
-				if (isVerbose)
-					printf("%i recieved %i packets from %i\n",i,recievedFromList[i][j],j);
-				if  (recievedFromList[i][j]!=numPackets)
-				{
-					if (isVerbose)
-					{
-						printf("Not all packets recieved. it was in reliable ordered mode so that means test failed or wait time needs increasing\n");
-
-						DebugTools::ShowError("Test failed.\n",!noPauses && isVerbose,__LINE__,__FILE__);
-					}
-					return 5;
-
-				}
-
-			}
-		}
-	}
-
-	printf("All packets recieved in order,pass\n");
-	return 0;
-
-}
-
-RakString EightPeerTest::GetTestName()
-{
-
-	return "EightPeerTest";
-
-}
-
-RakString EightPeerTest::ErrorCodeToString(int errorCode)
-{
-
-	switch (errorCode)
-	{
-
-	case 0:
-		return "No error";
-		break;
-
-	case 1:
-		return "Connect function returned failure.";
-		break;
-
-	case 2:
-		return "Peers failed to connect.";
-		break;
-
-	case 3:
-		return "There was a disconnection.";
-		break;
-
-	case 4:
-		return "Not ordered.";
-		break;
-
-	case 5:
-		return "Not reliable.";
-		break;
-
-	default:
-		return "Undefined Error";
-	}
-
-}
-
-EightPeerTest::EightPeerTest(void)
-{
-}
-
-EightPeerTest::~EightPeerTest(void)
-{
-}
-
-void EightPeerTest::DestroyPeers()
-{
-
-	int theSize=destroyList.Size();
-
-	for (int i=0; i < theSize; i++)
-		RakPeerInterface::DestroyInstance(destroyList[i]);
-
+#include <chrono>
+
+#include <gtest/gtest.h>
+
+#include "RakPeerInterface.h"
+#include "BitStream.h"
+#include "MessageIdentifiers.h"
+#include "RakSleep.h"
+
+using namespace RakNet;
+using Clock = std::chrono::steady_clock;
+
+static constexpr int NUM_PEERS = 8;
+static constexpr int NUM_PACKETS = 100;
+
+/**
+ * @brief Tests 8 peers fully connected in a mesh, each sending 100 reliable
+ * ordered packets to all others, verifying delivery and ordering.
+ */
+class EightPeerTest : public ::testing::Test {
+protected:
+    RakPeerInterface *peers[NUM_PEERS] = {};
+    unsigned short ports[NUM_PEERS] = {};
+
+    void SetUp() override {
+        for (int i = 0; i < NUM_PEERS; i++) {
+            peers[i] = RakPeerInterface::GetInstance();
+            ASSERT_NE(peers[i], nullptr);
+
+            SocketDescriptor sd(0, nullptr);
+            ASSERT_EQ(peers[i]->Startup(NUM_PEERS * 2, &sd, 1), RAKNET_STARTED);
+            peers[i]->SetMaximumIncomingConnections(NUM_PEERS);
+            ports[i] = peers[i]->GetMyBoundAddress().GetPort();
+        }
+    }
+
+    void TearDown() override {
+        for (int i = 0; i < NUM_PEERS; i++) {
+            if (peers[i])
+                RakPeerInterface::DestroyInstance(peers[i]);
+        }
+    }
+};
+
+TEST_F(EightPeerTest, FullMeshReliableOrdered) {
+    // Connect all peers to each other
+    for (int i = 0; i < NUM_PEERS; i++) {
+        for (int j = i + 1; j < NUM_PEERS; j++) {
+            ASSERT_EQ(peers[i]->Connect("127.0.0.1", ports[j], nullptr, 0),
+                      CONNECTION_ATTEMPT_STARTED)
+                << "Connect failed for peer " << i << " -> " << j;
+        }
+    }
+
+    // Wait for all peers to be fully connected (each should have NUM_PEERS-1 connections)
+    int connectionCount[NUM_PEERS] = {};
+    auto deadline = Clock::now() + std::chrono::seconds(20);
+    bool allConnected = false;
+
+    while (Clock::now() < deadline && !allConnected) {
+        for (int i = 0; i < NUM_PEERS; i++) {
+            for (Packet *p = peers[i]->Receive(); p;
+                 peers[i]->DeallocatePacket(p), p = peers[i]->Receive()) {
+                if (p->data[0] == ID_CONNECTION_REQUEST_ACCEPTED ||
+                    p->data[0] == ID_NEW_INCOMING_CONNECTION)
+                    connectionCount[i]++;
+                else if (p->data[0] == ID_CONNECTION_ATTEMPT_FAILED)
+                    FAIL() << "Peer " << i << " got CONNECTION_ATTEMPT_FAILED";
+                else if (p->data[0] == ID_NO_FREE_INCOMING_CONNECTIONS)
+                    FAIL() << "Peer " << i << " got NO_FREE_INCOMING_CONNECTIONS";
+            }
+        }
+
+        allConnected = true;
+        for (int i = 0; i < NUM_PEERS; i++) {
+            if (connectionCount[i] < NUM_PEERS - 1)
+                allConnected = false;
+        }
+        RakSleep(30);
+    }
+    ASSERT_TRUE(allConnected) << "Not all peers fully connected within timeout";
+
+    // Each peer sends NUM_PACKETS messages to all others
+    int receivedFrom[NUM_PEERS][NUM_PEERS] = {};
+    int expectedSeq[NUM_PEERS][NUM_PEERS] = {};
+
+    for (int k = 0; k < NUM_PACKETS; k++) {
+        for (int i = 0; i < NUM_PEERS; i++) {
+            BitStream bs;
+            bs.Write(static_cast<unsigned char>(ID_USER_PACKET_ENUM + 1));
+            bs.Write(k);
+            bs.Write(i);
+            peers[i]->Send(&bs, HIGH_PRIORITY, RELIABLE_ORDERED, 0,
+                           UNASSIGNED_SYSTEM_ADDRESS, true);
+        }
+        RakSleep(5);
+
+        // Drain while sending to avoid buffer buildup
+        for (int i = 0; i < NUM_PEERS; i++) {
+            for (Packet *p = peers[i]->Receive(); p;
+                 peers[i]->DeallocatePacket(p), p = peers[i]->Receive()) {
+                if (p->data[0] == ID_USER_PACKET_ENUM + 1) {
+                    BitStream bs(p->data, p->length, false);
+                    bs.IgnoreBytes(1);
+                    int seq, sender;
+                    bs.Read(seq);
+                    bs.Read(sender);
+                    if (sender >= 0 && sender < NUM_PEERS) {
+                        EXPECT_EQ(seq, expectedSeq[i][sender])
+                            << "Out of order: peer " << i << " from sender " << sender;
+                        expectedSeq[i][sender] = seq + 1;
+                        receivedFrom[i][sender]++;
+                    }
+                } else if (p->data[0] == ID_DISCONNECTION_NOTIFICATION ||
+                           p->data[0] == ID_CONNECTION_LOST) {
+                    FAIL() << "Peer " << i << " lost a connection during send phase";
+                }
+            }
+        }
+    }
+
+    // Drain remaining packets
+    auto recvDeadline = Clock::now() + std::chrono::seconds(5);
+    while (Clock::now() < recvDeadline) {
+        bool allDone = true;
+        for (int i = 0; i < NUM_PEERS; i++) {
+            for (Packet *p = peers[i]->Receive(); p;
+                 peers[i]->DeallocatePacket(p), p = peers[i]->Receive()) {
+                if (p->data[0] == ID_USER_PACKET_ENUM + 1) {
+                    BitStream bs(p->data, p->length, false);
+                    bs.IgnoreBytes(1);
+                    int seq, sender;
+                    bs.Read(seq);
+                    bs.Read(sender);
+                    if (sender >= 0 && sender < NUM_PEERS) {
+                        EXPECT_EQ(seq, expectedSeq[i][sender])
+                            << "Out of order: peer " << i << " from sender " << sender;
+                        expectedSeq[i][sender] = seq + 1;
+                        receivedFrom[i][sender]++;
+                    }
+                }
+            }
+            for (int j = 0; j < NUM_PEERS; j++) {
+                if (i != j && receivedFrom[i][j] < NUM_PACKETS)
+                    allDone = false;
+            }
+        }
+        if (allDone) break;
+        RakSleep(30);
+    }
+
+    // Verify all packets received
+    for (int i = 0; i < NUM_PEERS; i++) {
+        for (int j = 0; j < NUM_PEERS; j++) {
+            if (i != j) {
+                EXPECT_EQ(receivedFrom[i][j], NUM_PACKETS)
+                    << "Peer " << i << " received " << receivedFrom[i][j]
+                    << "/" << NUM_PACKETS << " from peer " << j;
+            }
+        }
+    }
 }
--- tests/MaximumConnectTest.cpp
+++ tests/MaximumConnectTest.cpp
@@ -3,258 +3,91 @@
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  LICENSE file in the root directory of this source tree. An additional grant
  *  of patent rights can be found in the PATENTS file in the same directory.
  *
  */
 
-#include "MaximumConnectTest.h"
+#include <chrono>
 
-/*
-What is being done here is having 8 peers all connect to eachother over the max defined connection.
+#include <gtest/gtest.h>
 
-It runs the connect, wait 20 seconds then see the current connections.
+#include "RakPeerInterface.h"
+#include "MessageIdentifiers.h"
+#include "RakSleep.h"
 
-Success conditions:
-All extra connections Refused.
+using namespace RakNet;
+using Clock = std::chrono::steady_clock;
 
-Failure conditions:
-There are more connected than allowed.
-The connect function fails, the test is not even done.
-GetMaximumIncomingConnections returns wrong value.
+static constexpr int NUM_PEERS = 8;
+static constexpr int MAX_CONNECTIONS = 4;
 
-RakPeerInterface Functions used, tested indirectly by its use:
-Startup
-Connect
-SetMaximumIncomingConnections
-Receive
-DeallocatePacket
-GetSystemList
+/**
+ * @brief Tests that SetMaximumIncomingConnections is enforced when many peers
+ * all try to connect to each other beyond the allowed limit.
+ */
+class MaximumConnectTest : public ::testing::Test {
+protected:
+    RakPeerInterface *peers[NUM_PEERS] = {};
+    unsigned short ports[NUM_PEERS] = {};
 
-RakPeerInterface Functions Explicitly Tested:
-SetMaximumIncomingConnections
-GetMaximumIncomingConnections 
+    void SetUp() override {
+        for (int i = 0; i < NUM_PEERS; i++) {
+            peers[i] = RakPeerInterface::GetInstance();
+            ASSERT_NE(peers[i], nullptr);
 
-*/
-int MaximumConnectTest::RunTest(DataStructures::List<RakString> params,bool isVerbose,bool noPauses)
-{
+            SocketDescriptor sd(0, nullptr);
+            ASSERT_EQ(peers[i]->Startup(MAX_CONNECTIONS, &sd, 1), RAKNET_STARTED);
+            peers[i]->SetMaximumIncomingConnections(MAX_CONNECTIONS);
+            ports[i] = peers[i]->GetMyBoundAddress().GetPort();
+        }
+    }
 
-	const int peerNum= 8;
-	const int maxConnections=4;//Max allowed connections for test
-	RakPeerInterface *peerList[peerNum];//A list of 8 peers
+    void TearDown() override {
+        for (int i = 0; i < NUM_PEERS; i++) {
+            if (peers[i])
+                RakPeerInterface::DestroyInstance(peers[i]);
+        }
+    }
+};
 
-	Packet *packet;
-	destroyList.Clear(false,_FILE_AND_LINE_);
-
-	int connReturn;
-	//Initializations of the arrays
-	for (int i=0;i<peerNum;i++)
-	{
-		peerList[i]=RakPeerInterface::GetInstance();
-		destroyList.Push(peerList[i],_FILE_AND_LINE_);
-
-		peerList[i]->Startup(maxConnections, &SocketDescriptor(60000+i,0), 1);
-		peerList[i]->SetMaximumIncomingConnections(maxConnections);
-
-		connReturn=peerList[i]->GetMaximumIncomingConnections();
-		if (connReturn!=maxConnections)
-		{
-			if (isVerbose)
-			{
-				printf("Getmaxconnections wrong for peer %i, %i should be the value but the value is %i.Fail\n",i,maxConnections,connReturn);
-
-				DebugTools::ShowError("",!noPauses && isVerbose,__LINE__,__FILE__);
-
-			}
-
-		}
-
-	}
-
-	//Connect all the peers together
-
-	for (int i=0;i<peerNum;i++)
-	{
-
-		for (int j=i+1;j<peerNum;j++)//Start at i+1 so don't connect two of the same together.
-		{
-
-			if (peerList[i]->Connect("127.0.0.1", 60000+j, 0,0)!=CONNECTION_ATTEMPT_STARTED)
-			{
-
-				if (isVerbose)
-					DebugTools::ShowError("Problem while calling connect.\n",!noPauses && isVerbose,__LINE__,__FILE__);
-
-				return 1;//This fails the test, don't bother going on.
-
-			}
-
-		}	
-
-	}
-
-	TimeMS entryTime=GetTimeMS();//Loop entry time
-
-	while(GetTimeMS()-entryTime<20000)//Run for 20 Secoonds
-	{
-
-		for (int i=0;i<peerNum;i++)//Receive for all peers
-		{
-
-			packet=peerList[i]->Receive();
-
-			if (isVerbose&&packet)
-				printf("For peer %i\n",i);
-
-			while(packet)
-			{
-				switch (packet->data[0])
-				{
-				case ID_REMOTE_DISCONNECTION_NOTIFICATION:
-					if (isVerbose)
-						printf("Another client has disconnected.\n");
-
-					break;
-				case ID_REMOTE_CONNECTION_LOST:
-					if (isVerbose)
-						printf("Another client has lost the connection.\n");
-
-					break;
-				case ID_REMOTE_NEW_INCOMING_CONNECTION:
-					if (isVerbose)              
-						printf("Another client has connected.\n");
-					break;
-				case ID_CONNECTION_REQUEST_ACCEPTED:
-					if (isVerbose)              
-						printf("Our connection request has been accepted.\n");
-
-					break;
-				case ID_CONNECTION_ATTEMPT_FAILED:
-					if (isVerbose)
-						printf("A connection has failed.\n");//Should happen in this test
-
-					break;
-
-				case ID_NEW_INCOMING_CONNECTION:
-					if (isVerbose)              
-						printf("A connection is incoming.\n");
-
-					break;
-				case ID_NO_FREE_INCOMING_CONNECTIONS:
-					if (isVerbose)              
-						printf("The server is full.\n");
-
-					break;
-
-				case ID_ALREADY_CONNECTED:
-					if (isVerbose)              
-						printf("Already connected\n");//Shouldn't happen
-
-					break;
-
-				case ID_DISCONNECTION_NOTIFICATION:
-					if (isVerbose)
-						printf("We have been disconnected.\n");
-					break;
-				case ID_CONNECTION_LOST:
-					if (isVerbose)
-						printf("Connection lost.\n");
-
-					break;
-				default:
-
-					break;
-				}
-
-				peerList[i]->DeallocatePacket(packet);
-
-				// Stay in the loop as long as there are more packets.
-				packet = peerList[i]->Receive();
-			}
-		}
-		RakSleep(0);//If needed for testing
-	}
-
-	DataStructures::List< SystemAddress  > systemList;
-	DataStructures::List< RakNetGUID > guidList;
-
-	for (int i=0;i<peerNum;i++)
-	{
-
-		peerList[i]->GetSystemList(systemList,guidList);
-		int connNum=guidList.Size();//Get the number of connections for the current peer
-		if (connNum>maxConnections)//Did we connect to more?
-		{
-
-			if (isVerbose)
-			{
-				printf("More connections were allowed to peer %i, %i total.Fail\n",i,connNum);
-
-				DebugTools::ShowError("",!noPauses && isVerbose,__LINE__,__FILE__);
-
-			}
-
-			return 2;
-
-		}
-
-	}
-
-	if (isVerbose)
-		printf("Pass\n");
-	return 0;
-
+TEST_F(MaximumConnectTest, GetMaximumIncomingConnections) {
+    for (int i = 0; i < NUM_PEERS; i++) {
+        EXPECT_EQ(peers[i]->GetMaximumIncomingConnections(), MAX_CONNECTIONS)
+            << "Peer " << i << " reports wrong max connections";
+    }
 }
 
-RakString MaximumConnectTest::GetTestName()
-{
+TEST_F(MaximumConnectTest, ExcessConnectionsRefused) {
+    // Every peer tries to connect to every other peer
+    for (int i = 0; i < NUM_PEERS; i++) {
+        for (int j = i + 1; j < NUM_PEERS; j++) {
+            ASSERT_EQ(peers[i]->Connect("127.0.0.1", ports[j], nullptr, 0),
+                      CONNECTION_ATTEMPT_STARTED)
+                << "Connect call failed for peer " << i << " -> " << j;
+        }
+    }
 
-	return "MaximumConnectTest";
+    // Wait for connections to settle
+    auto deadline = Clock::now() + std::chrono::seconds(10);
+    while (Clock::now() < deadline) {
+        for (int i = 0; i < NUM_PEERS; i++) {
+            for (Packet *p = peers[i]->Receive(); p;
+                 peers[i]->DeallocatePacket(p), p = peers[i]->Receive()) {
+            }
+        }
+        RakSleep(30);
+    }
 
-}
+    // Verify no peer has more connections than allowed
+    DataStructures::List<SystemAddress> systemList;
+    DataStructures::List<RakNetGUID> guidList;
 
-RakString MaximumConnectTest::ErrorCodeToString(int errorCode)
-{
-
-	switch (errorCode)
-	{
-
-	case 0:
-		return "No error";
-		break;
-
-	case 1:
-		return "The connect function failed";
-		break;
-
-	case 2:
-		return "An extra connection was allowed";
-		break;
-
-	case 3:
-		return "GetMaximumIncomingConnectionsn returned wrong value";
-		break;
-
-	default:
-		return "Undefined Error";
-	}
-
-}
-
-MaximumConnectTest::MaximumConnectTest(void)
-{
-}
-
-MaximumConnectTest::~MaximumConnectTest(void)
-{
-}
-
-void MaximumConnectTest::DestroyPeers()
-{
-
-	int theSize=destroyList.Size();
-
-	for (int i=0; i < theSize; i++)
-		RakPeerInterface::DestroyInstance(destroyList[i]);
-
+    for (int i = 0; i < NUM_PEERS; i++) {
+        peers[i]->GetSystemList(systemList, guidList);
+        int connCount = guidList.Size();
+        EXPECT_LE(connCount, MAX_CONNECTIONS)
+            << "Peer " << i << " has " << connCount
+            << " connections, max allowed is " << MAX_CONNECTIONS;
+    }
 }
